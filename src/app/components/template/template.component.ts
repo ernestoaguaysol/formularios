71560ee3492f10a647816105db3d2557342357a9
Ernesto <ernestoaguaysol@gmail.com>
2018-01-03 00:40:54 -0300

    check button static

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -15,8 +15,19 @@ export class TemplateComponent implements OnInit {
   usuario: Object = {
     nombre: null,
     apellido: null,
-    correo: null
+    correo: null,
+    pais: '',
+    sexo: 'Hombre'
   };
+
+  paises = [{
+    codigo: 'CRI',
+    nombre: 'Costa Rica'
+  },
+  {
+    codigo: 'ESP',
+    nombre: 'España'
+  }];
 
   constructor() { }
 
