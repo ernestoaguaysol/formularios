01280f6ad2d418bc7f367d3494245d6997dce37c
Ernesto <ernestoaguaysol@gmail.com>
2018-01-14 18:19:48 -0300

    this.forma.setValue this.forma.reset

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -8,8 +8,8 @@ import { FormGroup , FormControl , Validators } from '@angular/forms';
 })
 export class DataComponent {
 
-  usuario: Object = {
-    nombreCompleto: {
+  usuario: any = {
+    nombrecompleto: {
       nombre: 'ernesto',
       apellido: 'aguaysol'
     },
@@ -21,18 +21,26 @@ export class DataComponent {
   constructor() {
     this.forma = new FormGroup({
 
-      nombreCompleto: new FormGroup({
-        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)] ),
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('' , [Validators.required, Validators.minLength(3)] ),
         'apellido': new FormControl('', Validators.required)
       }),
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
+
+    this.forma.setValue( this.usuario );
   }
 
 
   guardarCambios() {
     console.log( this.forma.value );
     console.log(  this.forma );
-    
+    this.forma.reset( {
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    });
   }
 }
