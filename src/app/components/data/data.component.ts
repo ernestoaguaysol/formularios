bdc5d2e8fa92de82543f8de47b78649225894686
Ernesto <ernestoaguaysol@gmail.com>
2018-01-14 15:10:38 -0300

    formControlName in html and Validators in data.components.ts

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup , FormControl , Validator} from '@angular/forms';
+import { FormGroup , FormControl , Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -12,9 +12,9 @@ export class DataComponent {
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre': new FormControl('Ernesto'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl('', Validators.required ),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
     });
   }
 
