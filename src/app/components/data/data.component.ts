b0bd8aff0a8dbcf4177c4624735c5df66afd3ebd
Ernesto <ernestoaguaysol@gmail.com>
2018-01-15 12:03:04 -0300

    password and username

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup , FormControl , Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-data',
@@ -29,16 +30,48 @@ export class DataComponent {
       'correo': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
       'pasatiempos': new FormArray([
         new FormControl('correr', Validators.required )
-      ])
+      ]),
+      'username': new FormControl('', Validators.required, this.existeUsuario),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl()
     });
 
     // this.forma.setValue( this.usuario );
+    this.forma.controls['password2'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this)
+    ]);
   }
 
   agregarPasatiempo() {
     (<FormArray>this.forma.controls['pasatiempos']).push(
       new FormControl('', Validators.required )
     );
+  }
+
+  noIgual(control: FormControl): {[s: string]: boolean} {
+
+    if (control.value !== this.forma.controls['password1'].value ) {
+      return{
+        noiguales: true
+      };
+    }
+
+    return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any>|Observable<any>{
+    let promesa = new Promise((resolve , reject ) => {
+      setTimeout(() => {
+        if (control.value === 'strider') {
+          resolve( {existe: true} );
+        }else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+
+    return promesa;
   }
 
   noHerrera(control: FormControl): {[s:string]:boolean} {
